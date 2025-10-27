c7b9cfe1a675024f35e0289f5dbf35ddb708c085
Nicolás Vázquez <nvazquezs24@fpcoruna.afundacion.org>
2025-10-27 16:44:51 +0100

    Ej 1, Sprint 0 DI Terminado

---
+++ sprint0python.py
@@ -0,0 +1,12 @@
+def suma (num1, num2):
+    return num1 + num2
+def resta (num1, num2):
+    return num1 - num2
+def multiplicacion (num1, num2):
+    return num1 * num2
+def division (num1, num2):
+    if num1 == 0 or num2 == 0:
+        return 0
+    else:
+        return num1 / num2
+
