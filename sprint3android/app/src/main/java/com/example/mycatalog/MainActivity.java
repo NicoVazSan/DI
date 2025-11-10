58b121a145ba9f8c589a059bf14f85b7855c5261
Nicolás Vázquez <nvazquezs24@fpcoruna.afundacion.org>
2025-11-10 10:38:06 +0100

    Primer Boton

--- sprint3android/app/src/main/java/com/example/mycatalog/MainActivity.java
+++ sprint3android/app/src/main/java/com/example/mycatalog/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.mycatalog;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,5 +18,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        Button btnNavegar = findViewById(R.id.btnNavegarDetalle);
+
+        btnNavegar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Detail_activity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
