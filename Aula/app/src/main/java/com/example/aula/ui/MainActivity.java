8760df95f65af3068a5600f4a0003da663a90f37
Nicolás Vázquez <nvazquezs24@fpcoruna.afundacion.org>
2026-02-10 12:27:41 +0100

    Ejercicio 1

--- Aula/app/src/main/java/com/example/aula/ui/MainActivity.java
+++ Aula/app/src/main/java/com/example/aula/ui/MainActivity.java
@@ -20,7 +20,6 @@ public class MainActivity extends AppCompatActivity {
         );
         SplashScreen.installSplashScreen(this);
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
     }
 }
